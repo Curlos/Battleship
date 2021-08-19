d910ad579834a2f0da6b076c3ed5674289fef260
Carlos Martinez <curlosmart@gmail.com>
2021-08-19 16:14:26 -0400

    display gameboard with dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import Ship from './classes/Ship'
+import Gameboard from './classes/Gameboard'
+import Player from './classes/Player'
+
+const displayGameboard = (player) => {
+  const gameboards = document.querySelector('.gameboards')
+  const gameboard = document.createElement('div')
+  gameboard.classList.add('gameboard', player)
+  
+  for (let x = 0; x < 10; x++) {
+    
+    for(let y = 0; y < 10; y++) {
+      let positionElem = document.createElement('div')
+      positionElem.setAttribute('posX', x)
+      positionElem.setAttribute('posY', y)
+      gameboard.append(positionElem)
+    }
+  }
+
+  gameboards.append(gameboard)
+}
+
+displayGameboard('playerOne')
+
+displayGameboard('playerTwo')
